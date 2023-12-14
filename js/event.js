52e36f46fa3748e8d17b2f951e12d1c61c2ad233
Rahat <sirajrahat661@gmail.com>
2023-12-15 00:53:13 +0600

    Parallelogram and Pentagon showing some problem

--- js/event.js
+++ js/event.js
@@ -1,37 +1,102 @@
+var count = 1;
+var allGeometry ={};
 // triangle
-document.getElementById('triangle').addEventListener('click',function(){
+document.getElementById('triangle').addEventListener('click', function () {
     const base = getInputField('triangle-base');
-    const height = getInputField('triangle-height');  
-    const area = HalfMultiArea(base,height);
-})
+    const height = getInputField('triangle-height');
+    const area = HalfMultiArea(base, height);
+    if (check(allGeometry, 'triangle')) {
+        // if it exist then change that p tag
+        EditResult(area,'triangle','classTriangle');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.triangle = count ;
+        count++;
+        showResult(area,'triangle','classTriangle');
+    }
+});
 // rectangle
-document.getElementById('rectangle').addEventListener('click',function(){
-    const base = getInputField('rectangle-base');
-    const height = getInputField('rectangle-height');  
-    const area = FullMultiArea(base,height);
-})
-// parallelogram
-document.getElementById('parallelogram').addEventListener('click',function(){
-    const base = getInputField('parallelogram-base');
-    const height = getInputField('parallelogram-height');  
-    const area = FullMultiArea(base,height);
-})
-// rhombus
-document.getElementById('rhombus').addEventListener('click',function(){
+document.getElementById('rectangle').addEventListener('click', function () {
+    const base = getInputField('rectangle-width');
+    const height = getInputField('rectangle-height');
+    const area = FullMultiArea(base, height);
+    if (check(allGeometry, 'rectangle')) {
+        // if it exist then change that p tag
+        EditResult(area,'rectangle','classRectangle');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.rectangle = count ;
+        count++;
+        showResult(area,'rectangle','classRectangle');
+    }
     
+});
+// parallelogram
+document.getElementById('parallelogram').addEventListener('click', function () {
+    const base = getInputField('parallelogram-base');
+    const height = getInputField('parallelogram-height');
+    const area = FullMultiArea(base, height);
+    if (check(allGeometry, 'parallelogram')) {
+        // if it exist then change that p tag
+        EditResult(area,'parallelogram','classParallelogram');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.parallelogram= count ;
+        count++;
+        showResult(area,'parallelogram','ClassParallelogram');
+    }
+});
+// rhombus
+document.getElementById('rhombus').addEventListener('click', function () {
+
     const base = getInputField('major-diameter');
-    const height = getInputField('minor-diameter');  
-    const area = HalfMultiArea(base,height);
-})
+    const height = getInputField('minor-diameter');
+    const area = HalfMultiArea(base, height);
+    if (check(allGeometry, 'rhombus')) {
+        // if it exist then change that p tag
+        EditResult(area,'rhombus','classRhombus');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.rhombus = count ;
+        count++;
+        showResult(area,'rhombus','classRhombus');
+    }
+});
 // pentagon
-document.getElementById('pentagon').addEventListener('click',function(){
+document.getElementById('pentagon').addEventListener('click', function () {
     const base = getInputField('pentagon-perimeter');
-    const height = getInputField('pentagon-apothem');  
-    const area = HalfMultiArea(base,height);
-})
+    const height = getInputField('pentagon-apothem');
+    const area = HalfMultiArea(base, height);
+    if (check(allGeometry, 'pentagon')) {
+        // if it exist then change that p tag
+        EditResult(area,'pentagon','clasPentagon');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.pentagon = count ;
+        count++;
+        showResult(area,'pentagon','classPentagon');
+    }
+});
 // ellipse
-document.getElementById('ellipse').addEventListener('click',function(){
+document.getElementById('ellipse').addEventListener('click', function () {
     const base = getInputField('ellipse-semi-major');
-    const height = getInputField('ellipse-semi-minor');  
-    const area = HalfMultiArea(base,height);
-})
+    const height = getInputField('ellipse-semi-minor');
+    const area = HalfMultiArea(base, height);
+    if (check(allGeometry, 'ellipse')) {
+        // if it exist then change that p tag
+        EditResult(area,'ellipse','classEllipse');
+    }
+    else {
+        // if it doesn't exist
+        allGeometry.ellipse = count ;
+        count++;
+        showResult(area,'ellipse','classEllipse');
+    }
+});
+
+
--- js/utility.js
+++ js/utility.js
@@ -1,6 +1,25 @@
- function getInputField(fieldId){
+function getInputField(fieldId) {
     const id = document.getElementById(fieldId);
     const numberString = id.value;
     const number = parseFloat(numberString);
     return number;
- }
+}
+function check( allGeometry,shapeValue) {
+    if (Object.keys(allGeometry).indexOf(shapeValue) > -1) {
+        return true;
+    }
+    else
+        return false;
+}
+function showResult(area, geometry,className) {
+    const resultShower = document.getElementById('result-area');
+    const p = document.createElement('p');
+    p.innerText = allGeometry[geometry] + geometry + ' ' + area;
+    p.classList.add(className);
+    // console.log(p.classList);
+    resultShower.appendChild(p);
+}
+function EditResult(area, geometry,className) {
+    const p = document.getElementsByClassName(className);
+    p[0].innerText = allGeometry[geometry] + geometry + area;
+}
